fb879948fa8ae79dbb49664f76051ffa4e5fe0d2
vmartinez <vmartinez@redpoints.com>
2020-04-06 09:02:36 +0200

    Implement Srping Security

---
+++ src/main/java/com/example/newspaper/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.example.newspaper.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
+        this.userPrincipalDetailsService = userPrincipalDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+//                .antMatchers("/index.html").permitAll()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/categoria/tecnologia").authenticated()
+                .antMatchers("/categoria/videojuegos").hasRole("EDITOR")
+//                .antMatchers("/").hasRole("ADMIN")
+//                .antMatchers("/").hasRole("CREATE_POST")
+//                .antMatchers("/profile/**").authenticated()
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+//                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+//                .antMatchers("/api/v1/test1").hasAuthority("ACCESS_TEST1")
+//                .antMatchers("/api/v1/usuario").hasAuthority("ADMIN")
+//                .antMatchers("/api/v1/**").hasRole("ADMIN")
+                .antMatchers("/api/v1/usuario").permitAll()
+//                .antMatchers(HttpMethod.POST, "/api/v1/usuario").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
